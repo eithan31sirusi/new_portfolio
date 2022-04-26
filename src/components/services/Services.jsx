33b1d754702b7b229dfbe60cf686a47da5312cd8
eithan31sirusi <eithan26sirusi@gmail.com>
2022-04-27 00:30:15 +0300

    uploading the files to git and prceeding to netlify

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
+
+const Services = () => {
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+        </article>
+        {/* END OF UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+        </article>
+        {/* WEB DEVELOPMENT */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+        </article>
+        {/* END OF CONTENT CREATION */}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
